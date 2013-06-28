da419b411b9356c6b77424fd6216fedb85ebbeba
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2013-06-28 13:49:11 -0300

    Programa separado em diversos arquivos

---
+++ src/Syntax.ml
@@ -0,0 +1,19 @@
+
+(*  SINTAXE  *)
+
+type binop = Plus | Geq
+;;
+
+type tipo = Tbool | Tint | Tfun of tipo*tipo
+;;
+
+type term =
+|  Num   of int
+|  Bool  of bool
+|  Binop of binop*term*term
+|  If    of term*term*term
+|  Var   of string
+|  App   of term*term
+|  Fun   of string*tipo*term
+|  Let   of string*tipo*term*term
+;;
